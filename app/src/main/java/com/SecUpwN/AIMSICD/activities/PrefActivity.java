bcd8b6bf1809b30b1861eaa3b2c57b5d6169da90
Marvin Arnold <marvinmarnold@gmail.com>
2015-08-17 13:36:43 -0500

    Switch back and forth between modes

--- app/src/main/java/com/SecUpwN/AIMSICD/activities/PrefActivity.java
+++ app/src/main/java/com/SecUpwN/AIMSICD/activities/PrefActivity.java
@@ -7,16 +7,26 @@ package com.SecUpwN.AIMSICD.activities;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import com.SecUpwN.AIMSICD.AIMSICD;
+import com.SecUpwN.AIMSICD.R;
 import com.SecUpwN.AIMSICD.fragments.PrefFragment;
+import com.SecUpwN.AIMSICD.mapping.MappingActivitySafe;
+import com.SecUpwN.AIMSICD.service.AimsicdService;
 
 
 public class PrefActivity extends BaseActivity {
-
+    private final Context mContext = this;
+    private SharedPreferences prefs;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        prefs = mContext.getSharedPreferences( AimsicdService.SHARED_PREFERENCES_BASENAME, 0);
         loadFragment();
     }
 
@@ -24,6 +34,19 @@ public class PrefActivity extends BaseActivity {
     protected void onRestart() {
         super.onRestart();
         loadFragment();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (!prefs.getBoolean(getResources().getString(R.string.mapping_pref_expert_key), false)) {
+            Intent intent = new Intent(PrefActivity.this, MappingActivitySafe.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(PrefActivity.this, AIMSICD.class);
+            startActivity(intent);
+        }
     }
 
     private void loadFragment() {
